8b663f6484c249a7a3adafcdefbb90a013c641c0
OvenGuo0224 <ovenguo0224@gmail>
2018-03-07 09:18:53 +0800

    first commit

---
+++ id3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-03-07
+# @Author  : ${author} (${email})
+# @Link    : ${link}
+# @Version : $Id$
+
+import os
+
+def function():
+	pass
